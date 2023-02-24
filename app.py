8c48bb92d9fc065f945a366ab8885ce05f38b6d0
Moeshh <mex_ncn@hotmail.com>
2023-02-24 11:07:22 +0100

    test

--- app.py
+++ app.py
@@ -15,7 +15,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return rooster('all')
+    return '<p>Hello, World!</p>'
+    #return rooster('all')
 
 @app.route('/test/<zoekterm>')
 def test(zoekterm):
